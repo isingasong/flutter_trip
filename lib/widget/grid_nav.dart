22f8aac4d409d6b2cdc602934d75dccbdfbf7979
max <2416227158@qq.com>
2019-07-31 21:59:18 +0800

    second commit

--- lib/widget/grid_nav.dart
+++ lib/widget/grid_nav.dart
@@ -7,7 +7,7 @@ import 'package:flutter_trip/model/grid_nav_model.dart';
 class GridNav extends StatelessWidget {
   final GridNavModel gridNavModel;
 
-  const GridNav({Key key, this.gridNavModel}) : super(key: key);
+  const GridNav({Key key,@required this.gridNavModel}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container();
